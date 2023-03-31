a84dd63c217bb67bb4f5fb76c1834a048892c5fe
Pedro Vinícius <70115435+PiviaN@users.noreply.github.com>
2023-03-31 15:39:55 -0300

    forms ajuste mobile

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,24 +3,19 @@ import "./Form.css";
 
 export default function Form() {
   return (
-    <section className="form d-flex align-items-center">
+    <section className="form d-flex align-items-center ">
       <div className="d-flex flex-column">
         <h1 className="form-title">Lorem ipsum dolor sit amet.</h1>
         <p className="form-subtitle">Lorem ipsum dolor sit amet.</p>
 
-        <form>
-          <div className="row row-full">
-            <div className="col-12">
-              <input className="form-input" placeholder="Nome completo" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xl-6 col-12">
-              <input className="form-input" placeholder="E-mail" />
-            </div>
-            <div className="col-xl-6 col-12">
-              <input className="form-input" placeholder="(11) 91234-5678" />
-            </div>
+        <form className="d-flex flex-column">
+          <input className="form-input w-input" placeholder="Nome completo" />
+          <div className="split-desktop">
+            <input className="form-input w-input mr-8" placeholder="E-mail" />
+            <input
+              className="form-input w-input"
+              placeholder="(11) 91234-5678"
+            />
           </div>
         </form>
 
